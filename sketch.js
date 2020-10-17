d285d467a793bd059612c8d5c7e7d78364f4f478
Sanchita210507 <72611487+Sanchita210507@users.noreply.github.com>
2020-10-17 13:56:36 +0530

    Sketch File Modified

---
+++ sketch.js
@@ -0,0 +1,29 @@
+var edges;
+var fixedRect, movingRect;
+function setup() {
+  createCanvas(800,400);
+  fixedRect = createSprite(200,200,50,50);
+  fixedRect.shapeColor ="green";
+  fixedRect.debug=true;
+  movingRect = createSprite(500,200,50,100);
+  movingRect.shapeColor ="green";
+  movingRect.debug=true;
+}
+
+function draw() {
+  background(255,255,255); 
+  movingRect.x =mouseX;
+  movingRect.y = mouseY;
+  if(movingRect.x-fixedRect.x<movingRect.width/2+fixedRect.width/2
+    &&fixedRect.x-movingRect.x<fixedRect.width/2+movingRect.width/2
+    &&movingRect.y-fixedRect.y<movingRect.height/2+fixedRect.height/2
+    &&fixedRect.y-movingRect.y<fixedRect.height/2+movingRect.height/2){
+    fixedRect.shapeColor ="red";
+    movingRect.shapeColor = "red";
+  }else{
+    fixedRect.shapeColor ="green";
+    movingRect.shapeColor = "green";
+  }
+
+  drawSprites();
+}
